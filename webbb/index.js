5dffc78f804f7a3238c20e701036102d5be02b74
NathanCamilleri <95087638+NathanCamilleri@users.noreply.github.com>
2021-12-23 19:17:17 +0100

    Web design

---
+++ webbb/index.js
@@ -0,0 +1,8 @@
+    function Pass11() {
+        var textInBox4 = document.getElementById("testing").value;
+        var textInBox5 = document.getElementById("testing").value;
+        if (textInBox4 === nathan && textInBox5 === hello)
+            window.location.replace('about.html');
+        }else
+            alert("Wrong Username or Password, Pls Try Again");
+
